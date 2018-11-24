536778cb0fb5ed24df7c8b6d7f7973f20fbc4d36
Ianus Inferus <r_ex@163.com>
2018-11-24 16:39:16 +0800

    迁移到github，修正发布脚本，增加markdown风格README。 --- Move to github. Fix publish scripts. Add markdown README.

--- Publish.cmd
+++ Publish.cmd
@@ -10,6 +10,6 @@
 @call Clear.cmd
 @cd ..
 @del %PackName%.rar
-@rar a -av- -m5 -md4096 -tsm -tsc -s -k -t %PackName%.rar -x*\.svn -x*\.svn\* -x*.user -x*.suo Src Bin
+@rar a -av- -m5 -md4096 -tsm -tsc -s -k -t %PackName%.rar -x*\.svn -x*\.svn\* -x*\.git -x*\.git\* -x*.user -x*.suo Src Bin
 @if not exist Versions\ md Versions\
 @copy %PackName%.rar Versions\
