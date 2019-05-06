33df37dd52e9c9298b40ddf4afa917a439aa6d88
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2019-05-07 02:38:00 +0800

    修正构建脚本。 --- Fix build scripts.

--- Build.cmd
+++ Build.cmd
@@ -1,18 +1,38 @@
-PATH %windir%\Microsoft.NET\Framework\v4.0.30319;%PATH%
+@echo off
 
-MSBuild CommDevToolkit.sln /t:Rebuild /p:Configuration=Release
-MSBuild NeoRAGEx2002Tools.sln /t:Rebuild /p:Configuration=Release
+setlocal
+if "%SUB_NO_PAUSE_SYMBOL%"=="1" set NO_PAUSE_SYMBOL=1
+if /I "%COMSPEC%" == %CMDCMDLINE% set NO_PAUSE_SYMBOL=1
+set SUB_NO_PAUSE_SYMBOL=1
+call :main %*
+set EXIT_CODE=%ERRORLEVEL%
+if not "%NO_PAUSE_SYMBOL%"=="1" pause
+exit /b %EXIT_CODE%
 
-copy NDGrapher\SampleScr.vb ..\Bin\NDGrapher\
-copy Doc\Readme.*.txt ..\Bin\
-copy Doc\UpdateLog.*.txt ..\Bin\
-copy Doc\License.*.txt ..\Bin\
-copy Doc\License-NeoRAGEx2002.txt ..\Bin\
-copy zlib.net-license.txt ..\Bin\
-copy Doc\Localization.*.txt ..\Bin\
-del ..\Bin\Examples\ /S /F /Q
-IF NOT EXIST ..\Bin\Examples MD ..\Bin\Examples
-xcopy Examples\*.* ..\Bin\Examples\ /E
-IF NOT EXIST ..\Bin\Lan MD ..\Bin\Lan
-copy Lan\*.*.ini ..\Bin\Lan\
-pause
+:main
+for %%v in (2019 2017) do (
+  for %%p in (Enterprise Professional Community BuildTools) do (
+    for %%b in (Current 15.0) do (
+      if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe" (
+        set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe"
+        goto MSBuild_Found
+      )
+    )
+  )
+)
+:MSBuild_Found
+
+%MSBuild% CommDevToolkit.sln /t:Rebuild /p:Configuration=Release || exit /b 1
+%MSBuild% NeoRAGEx2002Tools.sln /t:Rebuild /p:Configuration=Release || exit /b 1
+
+copy Doc\Readme.*.txt ..\Bin\ || exit /b 1
+copy Doc\UpdateLog.*.txt ..\Bin\ || exit /b 1
+copy Doc\License.*.txt ..\Bin\ || exit /b 1
+copy Doc\License-NeoRAGEx2002.txt ..\Bin\ || exit /b 1
+copy Lib\zlib\zlib.net-license.txt ..\Bin\ || exit /b 1
+copy Doc\Localization.*.txt ..\Bin\ || exit /b 1
+if exist ..\Bin\Examples del ..\Bin\Examples\ /S /F /Q || exit /b 1
+if not exist ..\Bin\Examples md ..\Bin\Examples || exit /b 1
+xcopy Examples\*.* ..\Bin\Examples\ /E || exit /b 1
+if not exist ..\Bin\Lan md ..\Bin\Lan || exit /b 1
+copy Lan\*.*.ini ..\Bin\Lan\ || exit /b 1
