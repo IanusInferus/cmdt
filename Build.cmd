7844f4e789e71d2c7b09b32c1179aac8f77f7542
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2022-01-12 22:42:22 +0800

    将.Net Framework运行时的要求提升到4.8。 --- Requirement for .Net Framework is raised to 4.8.

--- Build.cmd
+++ Build.cmd
@@ -10,20 +10,24 @@ if not "%NO_PAUSE_SYMBOL%"=="1" pause
 exit /b %EXIT_CODE%
 
 :main
-for %%v in (2019 2017) do (
-  for %%p in (Enterprise Professional Community BuildTools) do (
-    for %%b in (Current 15.0) do (
-      if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe" (
-        set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe"
+for %%f in ("%ProgramFiles%" "%ProgramFiles(x86)%") do (
+  for %%v in (2022 2019) do (
+    for %%p in (Enterprise Professional Community BuildTools) do (
+      if exist "%%~f\Microsoft Visual Studio\%%v\%%p\MSBuild\Current\Bin\MSBuild.exe" (
+        set MSBuild="%%~f\Microsoft Visual Studio\%%v\%%p\MSBuild\Current\Bin\MSBuild.exe"
         goto MSBuild_Found
       )
     )
   )
 )
+echo MSBuild not found.
+echo You need to install Visual Studio 2019/2022 or add MSBuild environment variable.
+exit /b 1
 :MSBuild_Found
 
 %MSBuild% CommDevToolkit.sln /t:Rebuild /p:Configuration=Release || exit /b 1
 %MSBuild% NeoRAGEx2002Tools.sln /t:Rebuild /p:Configuration=Release || exit /b 1
+%MSBuild% REPLACE_ABI_APRIL.sln /t:Rebuild /p:Configuration=Release || exit /b 1
 
 copy Doc\Readme.*.txt ..\Bin\ || exit /b 1
 copy Doc\UpdateLog.*.txt ..\Bin\ || exit /b 1
